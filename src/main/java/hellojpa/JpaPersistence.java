0a804b521ace40d0adb40c72c0870e02cc761a8a
luke0408 <zjvl324@gmail.com>
2023-07-07 17:30:31 +0900

    :sparkles: [Feat]: detached (#3)

--- src/main/java/hellojpa/JpaPersistence.java
+++ src/main/java/hellojpa/JpaPersistence.java
@@ -19,12 +19,16 @@ public class JpaPersistence {
             /* [영속성 컨텍스트]
             - 비영속 : 영속성 컨텍스트와 전혀 관계가 없는 상태
             Member member = new Member();
+            em.remove(member); // 영속성 컨텍스트에서 삭제, DB에서도 삭제됨
 
             - 영속 : 영속성 컨텍스트에 관리되는 상태
             em.persist(member); // 영속성 컨텍스트에 연결, 이 시점에 DB에 저장되는 것이 아님
+            em.find(Member.class, 1L); // 데이터를 가져와서 영속성 컨텍스트에 저장
 
+            - 준영속 : 영속성 컨텍스트에 저장되었다가 분리된 상태, 영속성 컨텍스트가 제공하는 기능을 사용하지 못함
             em.detach(member); // 영속성 컨텍스트에서 분리
-            em.remove(member); // 영속성 컨텍스트에서 삭제, DB에서도 삭제됨
+            em.clear(); // 영속성 컨텍스트를 완전히 초기화
+            em.close(); // 영속성 컨텍스트를 종료
              */
 
             Member member1 = new Member(200L, "member200");
